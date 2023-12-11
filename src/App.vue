f64f0aa1abd78675d3c3f8602c17b110223c3714
upiw256 <bilqimlb@gmail.com>
2023-12-12 06:32:58 +0700

    fix koreksi jawaban

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <InsertAnswer />
+</template>
+<script setup>
+import InsertAnswer from "./components/InsertAnswer.vue";
+</script>
---
+++ src/components/InsertAnswer.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <!-- First input field with v-model to bind the value to the reactive variable -->
+    <input v-model="inputText1" placeholder="Input kunci Jawaban..." />
+
+    <!-- Second input field with v-model to bind the value to the reactive variable -->
+    <input v-model="inputText2" placeholder="Input Hasil siswa..." />
+
+    <!-- <p>Processed Text 1: {{ processedText1 }}</p>
+    <p>Processed Text 2: {{ processedText2 }}</p> -->
+
+    <p>Jumlah soal: {{ characterCount1 }}</p>
+    <p>Jumlah jawaban: {{ characterCount2 }}</p>
+
+    <!-- Display the number of characters that are the same -->
+    <p>Jawaban Yang Benar: {{ sameCharacterCount }}</p>
+
+    <!-- Display the positions of differing characters -->
+    <p v-if="differingPositions.length > 0">
+      Jawaban yang salah: No. {{ differingPositions.join(", ") }}
+    </p>
+    <p v-else>Tidak ada yang salah</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, watchEffect, computed } from "vue";
+
+// Reactive variables to store the input texts
+const inputText1 = ref("");
+const inputText2 = ref("");
+
+// Reactive variables to store the processed texts
+const processedText1 = ref("");
+const processedText2 = ref("");
+
+// Watch for changes in the inputText1 variable
+watchEffect(() => {
+  // Access the input text through the reactive variable
+  const textToProcess = inputText1.value;
+
+  // Perform any processing logic here
+  // For example, you can update the processedText1 variable
+  processedText1.value = `Processed: ${textToProcess}`;
+});
+
+// Watch for changes in the inputText2 variable
+watchEffect(() => {
+  // Access the input text through the reactive variable
+  const textToProcess = inputText2.value;
+
+  // Perform any processing logic here
+  // For example, you can update the processedText2 variable
+  processedText2.value = `Processed: ${textToProcess}`;
+});
+
+// Computed properties to calculate the character count for each text
+const characterCount1 = computed(() => {
+  return inputText1.value.length;
+});
+
+const characterCount2 = computed(() => {
+  return inputText2.value.length;
+});
+
+// Computed property to calculate the number of characters that are the same
+const sameCharacterCount = computed(() => {
+  const len = Math.min(inputText1.value.length, inputText2.value.length);
+  let count = 0;
+
+  for (let i = 0; i < len; i++) {
+    if (inputText1.value[i] === inputText2.value[i]) {
+      count++;
+    }
+  }
+
+  return count;
+});
+
+// Computed property to calculate the positions of differing characters
+const differingPositions = computed(() => {
+  const positions = [];
+  const len = Math.min(inputText1.value.length, inputText2.value.length);
+
+  for (let i = 0; i < len; i++) {
+    if (inputText1.value[i] !== inputText2.value[i]) {
+      positions.push(i + 1); // 1-based position
+    }
+  }
+
+  return positions;
+});
+</script>
